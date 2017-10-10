b9b90068e47bbd4b86c1a9597364774165b14901
celestialphineas <hanghang0713@hotmail.com>
2017-10-10 22:50:08 +0800

    Main UI frame.

--- app/src/main/java/io/github/celestialphineas/sanxing/MainActivity.java
+++ app/src/main/java/io/github/celestialphineas/sanxing/MainActivity.java
@@ -1,13 +1,34 @@
 package io.github.celestialphineas.sanxing;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        // Set the toolbar as the default action bar of the window
+        setSupportActionBar(toolbar);
+        // Get the view pager
+        ViewPager viewPager = (ViewPager) findViewById(R.id.main_view_pager);
+        // Get the tab layout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tab);
+        // Set the adapter
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        // Add view pagers
+        adapter.addFrag(new CountdownFrag(), getString(R.string.tab_tasks));
+        adapter.addFrag(new CountdownFrag(), getString(R.string.tab_habits));
+        adapter.addFrag(new CountdownFrag(), getString(R.string.tab_time_left));
+        // Binding
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        super.onCreate(savedInstanceState);
     }
 }
--- app/src/main/java/io/github/celestialphineas/sanxing/ViewPagerAdapter.java
+++ app/src/main/java/io/github/celestialphineas/sanxing/ViewPagerAdapter.java
@@ -1,8 +1,42 @@
 package io.github.celestialphineas.sanxing;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by celestialphineas on 17-10-10.
+ * This file is an adapter to the ViewPager
  */
 
-public class ViewPagerAdapter {
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return  mFragmentList.size();
+    }
+
+    public void addFrag(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
 }
