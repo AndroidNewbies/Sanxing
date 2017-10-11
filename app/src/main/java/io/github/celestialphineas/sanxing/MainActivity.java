93f6d5ffdec9010b74024afee2a17b8e7d5ad681
celestialphineas <hanghang0713@hotmail.com>
2017-10-11 21:31:58 +0800

    Fab added.

--- app/src/main/java/io/github/celestialphineas/sanxing/MainActivity.java
+++ app/src/main/java/io/github/celestialphineas/sanxing/MainActivity.java
@@ -1,21 +1,32 @@
 package io.github.celestialphineas.sanxing;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Get the toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         // Set the toolbar as the default action bar of the window
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        toolbar.setTitle(R.string.app_name);
         // Get the view pager
         ViewPager viewPager = (ViewPager) findViewById(R.id.main_view_pager);
         // Get the tab layout
@@ -26,9 +37,47 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFrag(new CountdownFrag(), getString(R.string.tab_tasks));
         adapter.addFrag(new CountdownFrag(), getString(R.string.tab_habits));
         adapter.addFrag(new CountdownFrag(), getString(R.string.tab_time_left));
-        // Binding
+        // Binding the view pager with the layout, as well as the fab button
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
-        super.onCreate(savedInstanceState);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(
+                    int position, float positionOffset, int positionOffsetPixels) {}
+            @Override
+            public void onPageSelected(int position) {
+                animateFab(position);
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) { }
+        });
+    }
+
+    private void animateFab(int tabPos) {
+        FloatingActionButton fabTasks = (FloatingActionButton) findViewById(R.id.fab_tasks);
+        FloatingActionButton fabHabits = (FloatingActionButton) findViewById(R.id.fab_habits);
+        FloatingActionButton fabTimeLeft = (FloatingActionButton) findViewById(R.id.fab_time_left);
+        switch (tabPos) {
+            case 0:
+                fabTasks.show();
+                fabHabits.hide();
+                fabTimeLeft.hide();
+                break;
+            case 1:
+                fabTasks.hide();
+                fabHabits.show();
+                fabTimeLeft.hide();
+                break;
+            case 2:
+                fabTasks.hide();
+                fabHabits.hide();
+                fabTimeLeft.show();
+                break;
+            default:
+                fabTasks.hide();
+                fabHabits.hide();
+                fabTimeLeft.hide();
+                break;
+        }
     }
 }
