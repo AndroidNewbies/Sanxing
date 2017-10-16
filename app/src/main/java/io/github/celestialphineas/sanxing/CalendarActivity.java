066ae23e3b91084e9acbe9767f54575adf688062
celestialphineas <hanghang0713@hotmail.com>
2017-10-16 12:33:35 +0800

    FAB animation added.

--- app/src/main/java/io/github/celestialphineas/sanxing/CalendarActivity.java
+++ app/src/main/java/io/github/celestialphineas/sanxing/CalendarActivity.java
@@ -2,6 +2,7 @@ package io.github.celestialphineas.sanxing;
 
 import android.app.ActionBar;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatSpinner;
@@ -11,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+
+import com.konifar.fab_transformation.FabTransformation;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -33,11 +36,21 @@ public class CalendarActivity extends AppCompatActivity {
                 finish();
             }
         });
+        // Spinner
         AppCompatSpinner spinner = (AppCompatSpinner) findViewById(R.id.toolbar_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.calendar_spinner_array,
                 R.layout.calendar_spinner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        //
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final Toolbar toolbarFooter = (Toolbar) findViewById(R.id.toolbar_footer);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FabTransformation.with(fab).transformTo(toolbarFooter);
+            }
+        });
     }
 }
