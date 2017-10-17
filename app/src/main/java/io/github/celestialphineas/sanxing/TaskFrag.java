53bc4c81fa21869849613f62b6e6650ae234d320
celestialphineas <hanghang0713@hotmail.com>
2017-10-17 22:38:31 +0800

    Recycler view with cards.

--- app/src/main/java/io/github/celestialphineas/sanxing/TaskFrag.java
+++ app/src/main/java/io/github/celestialphineas/sanxing/TaskFrag.java
@@ -3,9 +3,14 @@ package io.github.celestialphineas.sanxing;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -13,6 +18,7 @@ import android.view.ViewGroup;
  * Use the {@link TaskFrag#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class TaskFrag extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,7 +65,13 @@ public class TaskFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task, container, false);
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.task_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(new TaskRecyclerAdapter());
+        return view;
     }
 
 }
--- app/src/main/java/io/github/celestialphineas/sanxing/TaskRecyclerAdapter.java
+++ app/src/main/java/io/github/celestialphineas/sanxing/TaskRecyclerAdapter.java
@@ -1,8 +1,47 @@
 package io.github.celestialphineas.sanxing;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by celestialphineas on 17-10-17.
+ * Public class recycler view adapter for the task fragment
  */
 
-public class TaskRecyclerAdapter {
+public class TaskRecyclerAdapter
+        extends RecyclerView.Adapter<TaskRecyclerAdapter.TaskViewHolder> {
+    TaskRecyclerAdapter() {
+        // Empty constructor
+    }
+    class TaskViewHolder extends RecyclerView.ViewHolder {
+        TextView cardTitle;
+        TextView cardSubheading;
+        TaskViewHolder(View itemView) {
+            super(itemView);
+            cardTitle = itemView.findViewById(R.id.task_card_headline);
+            cardSubheading = itemView.findViewById(R.id.task_card_subheading);
+            cardTitle.setText("Hello world!");
+            cardSubheading.setText("Good night, my friend!");
+        }
+    }
+
+    @Override
+    public TaskViewHolder onCreateViewHolder(ViewGroup parent, int viewGroup) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.task_card, parent, false);
+        return new TaskViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(TaskViewHolder holder, int postion) {
+        // TODO: Bind the view with data
+    }
+
+    @Override
+    public int getItemCount() {
+        // TODO: Implement this, and return how many cards concerned
+        return 10;
+    }
 }
